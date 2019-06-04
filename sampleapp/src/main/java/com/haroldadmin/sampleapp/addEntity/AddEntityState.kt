9305f26a8811af01e9edc5f8d5f111dc670362d8
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-06-05 01:06:11 +0530

    feat: Handle state persistence in SavedEntityFragment

--- sampleapp/src/main/java/com/haroldadmin/sampleapp/addEntity/AddEntityState.kt
+++ sampleapp/src/main/java/com/haroldadmin/sampleapp/addEntity/AddEntityState.kt
@@ -1,9 +1,12 @@
 package com.haroldadmin.sampleapp.addEntity
 
+import android.os.Parcelable
 import com.haroldadmin.vector.VectorState
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class AddEntityState(
     val temporaryEntity: TemporaryEntity = TemporaryEntity(),
     val isSaving: Boolean = false,
     val isSaved: Boolean = false
-) : VectorState
+) : VectorState, Parcelable
--- sampleapp/src/main/java/com/haroldadmin/sampleapp/addEntity/TemporaryEntity.kt
+++ sampleapp/src/main/java/com/haroldadmin/sampleapp/addEntity/TemporaryEntity.kt
@@ -1,6 +1,10 @@
 package com.haroldadmin.sampleapp.addEntity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TemporaryEntity(
     val count: Int = 0,
     val name: String = ""
-)
+): Parcelable
