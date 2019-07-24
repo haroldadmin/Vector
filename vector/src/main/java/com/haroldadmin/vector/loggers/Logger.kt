c6a1d65044b35a61e92a36a35f7f0a1afd389682
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-07-24 19:00:18 +0530

    feat: Add docs to logger interface

--- vector/src/main/java/com/haroldadmin/vector/loggers/Logger.kt
+++ vector/src/main/java/com/haroldadmin/vector/loggers/Logger.kt
@@ -1,8 +1,22 @@
 package com.haroldadmin.vector.loggers
 
+/**
+ * Used to write debug logs.
+ *
+ * Implementations of this interface can be used to provide different output sources for log
+ * statements, such as Android's built in Logging class, or STDOUT.
+ */
 interface Logger {
 
+    /**
+     * A name tag associated with this logger for identification and filtering
+     */
     val tag: String
 
+    /**
+     * Logs the given message to the associated output
+     *
+     * @param message The message to be logged
+     */
     fun log(message: String)
 }
