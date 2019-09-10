31dc4d0ed852a6b912b057ea1374da18a5c2f380
Beer1234 <51640941+Beer1234@users.noreply.github.com>
2019-09-11 02:54:02 +0700

    Revert "[pull] master from haroldadmin:master"

---
+++ sampleapp/src/main/java/com/haroldadmin/sampleapp/addEntity/AddEntityViewModel.kt
@@ -0,0 +1,36 @@
+package com.haroldadmin.sampleapp.addEntity
+
+import androidx.lifecycle.viewModelScope
+import com.haroldadmin.sampleapp.CountingEntity
+import com.haroldadmin.sampleapp.repository.EntitiesRepository
+import com.haroldadmin.sampleapp.repository.getRandomColour
+import com.haroldadmin.vector.VectorViewModel
+import kotlinx.coroutines.launch
+
+class AddEntityViewModel(
+    initState: AddEntityState?,
+    private val entityRepository: EntitiesRepository
+) : VectorViewModel<AddEntityState>(initState ?: AddEntityState()) {
+
+    fun incrementCount() = viewModelScope.launch {
+        setState { copy(temporaryEntity = TemporaryEntity(count = currentState.temporaryEntity.count + 1)) }
+    }
+
+    fun decrementCount() = viewModelScope.launch {
+        setState { copy(temporaryEntity = TemporaryEntity(count = currentState.temporaryEntity.count - 1)) }
+    }
+
+    fun saveEntity(name: String) = viewModelScope.launch {
+        setState { copy(isSaving = true, isSaved = false) }
+        withState { state ->
+            val entity = state.temporaryEntity.copy(name = name).toEntity()
+            entityRepository.saveNewEntity(entity)
+            setState { copy(isSaved = true, isSaving = false) }
+        }
+    }
+
+    private fun TemporaryEntity.toEntity(): CountingEntity {
+        val colour = getRandomColour()
+        return CountingEntity.Impl(name = this.name, counter = this.count, colour = colour)
+    }
+}
